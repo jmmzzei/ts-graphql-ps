d609c7be2f96e5ec953bbb61cc22afee2f9cd643
jmm <josemmazzei@gmail.com>
2020-06-27 19:41:00 -0300

    Add deleteProduct to the API

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,17 +1,23 @@
 import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, BaseEntity} from 'typeorm'
+import {Field, ObjectType} from 'type-graphql'
 
+@ObjectType()
 @Entity()
 export class Product extends BaseEntity{
 
+  @Field()
   @PrimaryGeneratedColumn()
    id!: number
 
+  @Field()
   @Column()
    name!: string
 
+  @Field()
   @Column('int', {default: 0})
    quantity!: number
 
+  @Field()
   @CreateDateColumn({type: "timestamp"})
    createdAt!:string
 }
--- src/resolvers/ProductResolver.ts
+++ src/resolvers/ProductResolver.ts
@@ -1,15 +1,27 @@
-import {Resolver, Query, Mutation, Arg} from 'type-graphql'
+import {Resolver, Query, Mutation, Arg, Int} from 'type-graphql'
 import {Product} from '../entity/Product'
 
 @Resolver()
-export class ProductResolver{
-  @Mutation(()=> Boolean)
-  async createProducts(
-   @Arg('name') name: string,
-   @Arg('quantity') quantity: number
-  ){
-    await Product.insert({name, quantity})
-    console.log(name, quantity)
-    return true
-  }
+export class ProductResolver {
+    @Mutation(() => Boolean)
+    async createProducts(
+        @Arg('name') name: string,
+        @Arg('quantity') quantity: number
+    ) {
+        await Product.insert({ name, quantity })
+        console.log(name, quantity)
+        return true
+    }
+
+    @Query(() => [Product])
+    getProducts() {
+        return Product.find()
+    }
+
+    @Mutation(() => Boolean)
+    async deleteProduct(@Arg('id', () => Int) id: number) {
+        const response = await Product.delete(id)
+        return response.affected === 1 ? true : false
+    }
+
 }
